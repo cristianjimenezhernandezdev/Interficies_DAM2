543e899fc8b8f1a36416e6632cab6e4c52495a10
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-11-19 18:22:12 +0100

    Added changes to ComboBox and new Radi project

--- ComboBox/Combo_Box/Combo_Box/MainWindow.xaml.cs
+++ ComboBox/Combo_Box/Combo_Box/MainWindow.xaml.cs
@@ -30,6 +30,46 @@ namespace Combo_Box
             llistaPoblacions.Add(new Poblacions() { NomPoblacions = "Girona" });
             Poblacions.ItemsSource = llistaPoblacions;
         }
+   
+        private void TotesP_UChecked(object sender, RoutedEventArgs e)
+        {
+            Espanya.IsChecked = false;
+            China.IsChecked = false;
+            Italia.IsChecked = false;
+            UK.IsChecked = false;
+            Portugal.IsChecked = false;
+        }
+
+        private void TotesP_Checked_1(object sender, RoutedEventArgs e)
+        {
+            Espanya.IsChecked = true;
+            China.IsChecked = true;
+            Italia.IsChecked = true;
+            UK.IsChecked = true;
+            Portugal.IsChecked = true;
+        }
+        private void Idividual_Checked(object sender, RoutedEventArgs c)
+        {
+            if(Espanya.IsChecked==true  && UK.IsChecked==true && Italia.IsChecked==true && China.IsChecked==true && Portugal.IsChecked == true)
+            {
+                TotesP.IsChecked = true;
+            }
+            else
+            {
+                TotesP.IsChecked=null;
+            }
+        }
+        private void Idividual_UChecked(object sender, RoutedEventArgs c)
+        {
+            if (Espanya.IsChecked == false && UK.IsChecked == false && Italia.IsChecked == false && China.IsChecked == false && Portugal.IsChecked == false)
+            {
+                TotesP.IsChecked = false;
+            }
+            else
+            {
+                TotesP.IsChecked = null;
+            }
+        }
     }
     public class Poblacions
     {
